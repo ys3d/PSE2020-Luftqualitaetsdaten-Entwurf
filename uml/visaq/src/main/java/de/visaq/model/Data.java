fc6bddb480d54dab5a89eaa38242cc02f70413b0
Daniel Schild <daniel.schild@student.kit.edu>
2020-06-10 12:10:15 +0200

    Adds first Data-Structure

--- uml/visaq/src/main/java/de/visaq/model/Data.java
+++ uml/visaq/src/main/java/de/visaq/model/Data.java
@@ -1,5 +1,13 @@
 package de.visaq.model;
 
-public interface Data {
+import de.visaq.model.sensorthings.Location;
+import de.visaq.model.sensorthings.ObservedProperty;
 
+public interface Data {
+	public Location getLocation();
+	
+	public ObservedProperty getObservedProperty();
+	
+	public double getValue();
+	
 }
--- uml/visaq/src/main/java/de/visaq/model/SensorData.java
+++ uml/visaq/src/main/java/de/visaq/model/SensorData.java
@@ -1,5 +1,23 @@
 package de.visaq.model;
 
+import de.visaq.model.sensorthings.Location;
+import de.visaq.model.sensorthings.ObservedProperty;
+
 public class SensorData implements Data{
+
+	public Location getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ObservedProperty getObservedProperty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
