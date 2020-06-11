ac4d0bddff2f9d0c9152d609aa661d0d22874494
Katharina Biernacka <kath.biernacka@gmail.com>
2020-06-11 15:47:00 +0200

    added Legend class

---
+++ uml/visaq/src/main/java/de/visaq/view/elements/map/Legend.java
@@ -0,0 +1,23 @@
+package de.visaq.view.elements.map;
+
+import java.awt.Color;
+
+public class Legend {
+
+	Color PRIMARYCOLOR;
+	Color SECONDARYCOLOR;
+	
+	public void show() {
+		
+		// TODO implement
+		
+	}
+	
+	@SuppressWarnings("unused")
+	private void gradient() {
+		
+		// TODO implement
+		
+	}
+	
+}
