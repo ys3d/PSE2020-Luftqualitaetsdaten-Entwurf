b77f9ff05f921b4c4cd94f23c70d1fea68cc957b
Katharina Biernacka <kath.biernacka@gmail.com>
2020-06-13 09:59:08 +0200

    added descriptions

---
+++ uml/visaq/src/main/java/de/visaq/view/elements/AirQualityData.java
@@ -0,0 +1,9 @@
+package de.visaq.view.elements;
+
+import java.awt.Color;
+
+public abstract class AirQualityData {
+	
+	public AirQualityData(String name, Color primaryColor, Color secondaryColor) {
+	}
+}
