f8d84aa3b1df99a21d8df7d18181f3fd6425afff
Katharina Biernacka <kath.biernacka@gmail.com>
2020-06-15 21:29:04 +0200

    didnt safe change and had to recommit

--- uml/visaq/src/main/java/de/visaq/view/elements/AirQuality/AirQualityData.java
+++ uml/visaq/src/main/java/de/visaq/view/elements/AirQuality/AirQualityData.java
@@ -9,23 +9,14 @@ import java.awt.Color;
  */
 
 public abstract class AirQualityData {
-	/*
-	 * Name of the data type
-	 */
-	private String name;
-	/*
-	 * Primary color used to portray the overlay
-	 */
-	private Color primaryColor;
-	/*
-	 * secondary color used to portray the overlay
-	 */
-	private Color secondaryColor;
-	
-	/*
-	 * Constructor constructing the data
-	 */
 
+	private String name;
+	private Color primaryColor;
+	private Color secondaryColor;
+
+	/*
+	 * Constructor for the AirQualitydata used for the coloration of the legend and the interpolation
+	 */
 	public AirQualityData(String name, Color primaryColor, Color secondaryColor) {
 		this.name = name;
 		this.primaryColor = primaryColor;
@@ -33,29 +24,51 @@ public abstract class AirQualityData {
 	}
 
 	/*
-	 * Getter and setters used for the attitudes
+	 * getter for the name of the data type
+	 * @return name of the type
 	 */
-
 	public String getName() {
 		return name;
 	}
-
+	
+	/*
+	 * setter for the name of the data type
+	 * @param name, name of the type
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/*
+	 * getter for the primary color of the data type
+	 * @return primary color
+	 */
 	public Color getPrimaryColor() {
 		return primaryColor;
 	}
 
-	public void setPrimaryColor(Color primaryColor) {
+	/*
+	 * setter for the primary color of the type
+	 * @param primarycolor of the type when shown as legend or 
+	 * interpolation
+	 */
+	private void setPrimaryColor(Color primaryColor) {
 		this.primaryColor = primaryColor;
 	}
 
+	/*
+	 * getter for the name of the data type
+	 * @return secondaryColor
+	 */
 	public Color getSecondaryColor() {
 		return secondaryColor;
 	}
 
+	/*
+	 * setter for the secondary color
+	 * @param secondaryColor, secondary color of the type when shown as legend or 
+	 * interpolation
+	 */
 	public void setSecondaryColor(Color secondaryColor) {
 		this.secondaryColor = secondaryColor;
 	}
