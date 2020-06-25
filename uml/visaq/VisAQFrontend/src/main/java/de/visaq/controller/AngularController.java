8748ed5d6feabcfdd323c6f5cbadb6eaf86fbd09
Frizzy-M <maria.kraus1@web.de>
2020-06-25 13:37:57 +0200

    Added Katharinas AngularController.

--- uml/visaq/VisAQFrontend/src/main/java/de/visaq/controller/AngularController.java
+++ uml/visaq/VisAQFrontend/src/main/java/de/visaq/controller/AngularController.java
@@ -1,5 +1,28 @@
 package de.visaq.controller;
 
-public class AngularController {
+import de.visaq.view.VisAQ;
+import def.angular.core.NgModule;
+import def.angular.http.Request;
+import def.angular.platform_browser.BrowserModule;
 
+@NgModule(imports = { BrowserModule.class }, declarations = { VisAQ.class }, // Appmodule root
+        bootstrap = { VisAQ.class } // Appmodule root
+)
+
+/**
+ * Controller that requests data from the server
+ */
+public class AngularController {
+    private Request request = new Request();
+
+    /**
+     * The request to the Server is send here through an angular application. It returns a json data
+     * that is parsed to a Sensorthing data
+     * 
+     * @return the Sensorthing used in the view
+     */
+    public synchronized SensorThing sendRequest() {
+        request.json();
+        return null;
+    }
 }
