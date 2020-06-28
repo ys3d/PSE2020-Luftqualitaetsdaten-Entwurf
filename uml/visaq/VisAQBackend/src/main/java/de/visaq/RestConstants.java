3500a92b9649101b33f0504a63726eaf11e68afc
Fabian Reinbold <fabian.reinbold@outlook.com>
2020-06-28 13:31:50 +0200

    Add restart ability

--- uml/visaq/VisAQBackend/src/main/java/de/visaq/RestConstants.java
+++ uml/visaq/VisAQBackend/src/main/java/de/visaq/RestConstants.java
@@ -1,10 +1,25 @@
 package de.visaq;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Contains constants that are used in REST communication.
  */
 public final class RestConstants {
-    public static final String ENTRY_POINT = "http://api.smartaq.net/v1.0";
+    public static final String ENTRY_POINT;
+
+    static {
+        String entryPoint;
+        Properties p = new Properties();
+        try {
+            p.load(RestConstants.class.getResourceAsStream("/rest_constants.properties"));
+            entryPoint = p.getProperty("entry_point");
+        } catch (IOException e) {
+            entryPoint = null;
+        }
+        ENTRY_POINT = entryPoint;
+    }
 
     private RestConstants() {
         throw new IllegalStateException();
